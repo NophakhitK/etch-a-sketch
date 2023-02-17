c78ab94420a959343db69363c21e0855919be29b
Nophakhit Khositchaiphithak <nophakhit.k@gmail.com>
2023-02-17 15:38:13 +0700

    Added reset function and grid size function

--- js/script.js
+++ js/script.js
@@ -1,39 +1,46 @@
+const gridContainer = document.getElementById('gridContainer');
+let numberOfRow = document.querySelector('#myRange').value;
+let numberOfColumn = document.querySelector('#myRange').value;
+const resetButton = document.querySelector('.resetBtn');
 
-// let columns = 4;
-// let rows = 4;
+const slider = document.getElementById("myRange");
+const output = document.getElementById("gridNumber");
+output.innerHTML = `${slider.value} * ${slider.value}`;
 
-// let grid = document.createElement('div');
-// grid.className = 'grid';
-// for (let i = 0; i < columns; ++i) {
-//     let column = document.createElement('div'); // create column
-//     column.className = 'column';
-//     for (let j = 0; j < rows; ++j) {
-//         let row = document.createElement('div'); // create row
-//         row.className = 'row';
-//         column.appendChild(row); // append row in column
-//     }
-//     grid.appendChild(column); // append column inside grid
-// }
-// document.body.appendChild(grid);
+slider.oninput = function () {
+    output.innerHTML = `${slider.value} * ${slider.value}`;
+}
 
+makeGrid();
 
-const gridContainer = document.getElementById('container');
-const numberOfRow = 16;
-const numberOfColumn = 16;
-
-
-for (let i = 0; i < numberOfRow; ++i) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    gridContainer.appendChild(row);
-    for (let c = 0; c < numberOfColumn; ++c) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        row.appendChild(cell);
-        cell.addEventListener('mouseover', function (e) {
-            if (e.target.matches('.cell')) {
-                cell.classList.add('colorCell')
-            }
-        });
+function makeGrid() {
+    for (let i = 0; i < numberOfRow; ++i) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        gridContainer.appendChild(row);
+        for (let c = 0; c < numberOfColumn; ++c) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            row.appendChild(cell);
+            cell.addEventListener('mouseover', function (e) {
+                if (e.target.matches('.cell')) {
+                    cell.classList.add('colorCell');
+                    resetButton.addEventListener('click', () => {
+                        cell.classList.remove("colorCell");
+                        resetGame();
+                    });
+                }
+            });
+        }
     }
 }
+
+function resetGame() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+    newSliderValue = document.querySelector('#myRange').value;
+    numberOfColumn = newSliderValue;
+    numberOfRow = newSliderValue;
+    makeGrid();
+};
