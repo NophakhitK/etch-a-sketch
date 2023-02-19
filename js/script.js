6eba40b22a0a347ebcf65fe78ee8089a2268f268
Nophakhit Khositchaiphithak <nophakhit.k@gmail.com>
2023-02-19 13:11:50 +0700

    Refactored function for more readability and faster performance

--- js/script.js
+++ js/script.js
@@ -1,15 +1,39 @@
 const gridContainer = document.getElementById('gridContainer');
 let numberOfRow = document.querySelector('#myRange').value;
 let numberOfColumn = document.querySelector('#myRange').value;
+
+
 const resetButton = document.querySelector('.resetBtn');
+
+let row;
+let cells = document.getElementsByClassName('cell');
 
 const slider = document.getElementById("myRange");
 const output = document.getElementById("gridNumber");
 output.innerHTML = `${slider.value} * ${slider.value}`;
+slider.oninput = function () {
+    output.innerHTML = `${slider.value} * ${slider.value}`;
+}
 
 let allTools = document.querySelectorAll('.tools');
-
+let colorPicker = document.querySelector('#colorPicker').value
 let toggleColorMode = document.querySelector('#colorMode');
+let toggleRainbowMode = document.querySelector('#rainbowMode');
+
+resetButton.addEventListener('click', () => {
+    resetGame();
+});
+
+for (let i = 0; i < numberOfRow; ++i) {
+    row = document.createElement('div');
+    row.classList.add('row');
+    gridContainer.appendChild(row);
+    for (let c = 0; c < numberOfColumn; ++c) {
+        cell = document.createElement('div');
+        cell.classList.add('cell');
+        row.appendChild(cell);
+    }
+}
 
 function tglColorMode() {
     if (!toggleColorMode.classList.contains('toggled')) {
@@ -19,8 +43,6 @@ function tglColorMode() {
     }
 };
 
-let toggleRainbowMode = document.querySelector('#rainbowMode');
-
 function tglRainbowMode() {
     if (!toggleRainbowMode.classList.contains('toggled')) {
         toggleRainbowMode.classList.add('toggled');
@@ -28,9 +50,6 @@ function tglRainbowMode() {
         toggleRainbowMode.classList.remove('toggled');
     }
 }
-
-
-
 
 function generateRandomRGB() {
     let RGBColor1 = Math.floor(Math.random() * 256);
@@ -40,38 +59,15 @@ function generateRandomRGB() {
     return RGBColor;
 }
 
-
-slider.oninput = function () {
-    output.innerHTML = `${slider.value} * ${slider.value}`;
-}
-
-let colorPicker = document.querySelector('#colorPicker').value
-
-resetButton.addEventListener('click', () => {
-    resetGame();
-});
-
-makeGrid();
-
-function makeGrid() {
-    for (let i = 0; i < numberOfRow; ++i) {
-        const row = document.createElement('div');
+function makeNewGrid() {
+    for (i = 0; i < numberOfRow; ++i) {
+        row = document.createElement('div');
         row.classList.add('row');
         gridContainer.appendChild(row);
-        for (let c = 0; c < numberOfColumn; ++c) {
-            const cell = document.createElement('div');
+        for (c = 0; c < numberOfColumn; ++c) {
+            cell = document.createElement('div');
             cell.classList.add('cell');
             row.appendChild(cell);
-            cell.addEventListener('mouseover', function (e) {
-                if ((toggleColorMode.classList.contains('toggled')) && (e.target.matches('.cell'))) {
-                    pickColor();
-                    cell.style.backgroundColor = colorPicker;
-
-                } else if ((toggleRainbowMode.classList.contains('toggled')) && (e.target.matches('.cell'))) {
-                    RGBColor = generateRandomRGB();
-                    cell.style.backgroundColor = RGBColor;
-                }
-            });
         }
     }
 }
@@ -83,7 +79,8 @@ function resetGame() {
     newSliderValue = document.querySelector('#myRange').value;
     numberOfColumn = newSliderValue;
     numberOfRow = newSliderValue;
-    makeGrid();
+    makeNewGrid();
+    colorCell();
 };
 
 function pickColor() {
@@ -94,3 +91,59 @@ function checkMode() {
     allTools.classList.remove('toggled');
 }
 
+// makeGrid();
+
+// function makeGrid() {
+//     for (let i = 0; i < numberOfRow; ++i) {
+//         const row = document.createElement('div');
+//         row.classList.add('row');
+//         gridContainer.appendChild(row);
+//         for (let c = 0; c < numberOfColumn; ++c) {
+//             const cell = document.createElement('div');
+//             cell.classList.add('cell');
+//             row.appendChild(cell);
+//             cell.addEventListener('mouseover', function (e) {
+//                 if ((toggleColorMode.classList.contains('toggled')) && (e.target.matches('.cell'))) {
+//                     pickColor();
+//                     cell.style.backgroundColor = colorPicker;
+
+//                 } else if ((toggleRainbowMode.classList.contains('toggled')) && (e.target.matches('.cell'))) {
+//                     RGBColor = generateRandomRGB();
+//                     cell.style.backgroundColor = RGBColor;
+//                 }
+//             });
+//         }
+//     }
+// }
+
+
+// 
+
+// for (let w = 0; w < 1000; ++w) {
+//     cells[0].addEventListener('mouseover', () => {
+//         if (toggleColorMode.classList.contains('toggled')) {
+//             console.log("heard");
+//             pickColor();
+//             cells[0].style.backgroundColor = colorPicker;
+//         } else if (toggleRainbowMode.classList.contains('toggled')) {
+//             RGBColor = generateRandomRGB();
+//             cells[0].style.backgroundColor = RGBColor;
+//         }
+//     });
+// }s
+
+function colorCell() {
+    for (const cell of cells) {
+        cell.addEventListener('mouseover', () => {
+            if (toggleColorMode.classList.contains('toggled')) {
+                pickColor();
+                cell.style.backgroundColor = colorPicker;
+            } else if (toggleRainbowMode.classList.contains('toggled')) {
+                RGBColor = generateRandomRGB();
+                cell.style.backgroundColor = RGBColor;
+            }
+        })
+    }
+}
+
+colorCell();
